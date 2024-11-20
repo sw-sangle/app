0ffbffd325e9336be2c3ae6b3a57a40d64f24f85
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-11-20 11:35:27 +0900

    feat: typography

---
+++ Sangle/Constant/Network.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  Pickr
+//
+//  Created by  jwkwon0817 on 11/11/24.
+//
+
+enum Network {
+    static let baseUrl = "not defined"
+}
---
+++ Sangle/Extension/Typography.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+
+struct Typography {
+    enum Style {
+        case title1, title2, subtitle1, subtitle1Emphasized, subtitle2, subtitle2Emphasized, body1, body1Emphasized, body2, body2Emphasized
+        
+        var fontSize: CGFloat {
+            switch self {
+            case .title1: return 24
+            case .title2: return 20
+            case .subtitle1, .subtitle1Emphasized: return 18
+            case .subtitle2, .subtitle2Emphasized: return 16
+            case .body1, .body1Emphasized: return 14
+            case .body2, .body2Emphasized: return 13
+            }
+        }
+        
+        var lineHeight: CGFloat {
+            switch self {
+            case .title1: return 32
+            case .title2: return 28
+            case .subtitle1, .subtitle1Emphasized: return 24
+            case .subtitle2, .subtitle2Emphasized: return 22
+            case .body1, .body1Emphasized: return 20
+            case .body2, .body2Emphasized: return 18
+            }
+        }
+        
+        var letterSpacing: CGFloat {
+            let ratio = 0.0
+            
+            return fontSize * ratio
+        }
+        
+        var fontName: String {
+            switch self {
+            case .title1, .title2, .subtitle1Emphasized, .subtitle2Emphasized, .body1Emphasized, .body2Emphasized:
+                return "WantedSans-SemiBold"
+            case .subtitle1, .subtitle2, .body1, .body2:
+                return "WantedSans-Medium"
+            }
+        }
+    }
+}
+
+// Typography Environment Key
+struct TypographyEnvironmentKey: EnvironmentKey {
+    static let defaultValue: (Typography.Style, Color) -> Font = { style, color in
+        return Font.custom(style.fontName, size: style.fontSize)
+    }
+}
+
+extension EnvironmentValues {
+    var typography: (Typography.Style, Color) -> Font {
+        get { self[TypographyEnvironmentKey.self] }
+        set { self[TypographyEnvironmentKey.self] = newValue }
+    }
+}
+
+struct TypographyModifier: ViewModifier {
+    let style: Typography.Style
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom(style.fontName, size: style.fontSize))
+            .foregroundStyle(color)
+            .lineSpacing((style.lineHeight - style.fontSize) / 2)
+            .padding(.vertical, (style.lineHeight - style.fontSize) / 4)
+            .kerning(style.letterSpacing)
+            .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+extension View {
+    func typography(_ style: Typography.Style, color: Color = .primary) -> some View {
+        self.modifier(TypographyModifier(style: style, color: color))
+    }
+}
