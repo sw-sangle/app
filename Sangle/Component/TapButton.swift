23c13d85573c0a699c0efee6d9288a548124492e
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-11-20 11:57:05 +0900

    feat: tap button component

---
+++ Sangle/Component/TapButton.swift
@@ -0,0 +1,49 @@
+//
+//  Button.swift
+//  Sangle
+//
+//  Created by  jwkwon0817 on 11/20/24.
+//
+
+import SwiftUI
+
+enum TapButtonType {
+    case primary, secondary
+}
+
+enum TapButtonSize {
+    case small, large
+}
+
+struct TapButton: View {
+    let action: () -> Void
+    let text: String
+    let type: TapButtonType
+    let size: TapButtonSize
+    
+    @Binding var disabled: Bool
+    
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .typography(
+                    size == .small ? .body2Emphasized : .subtitle2Emphasized, color: disabled ? Color(hex: "3C3C43", alpha: 0.3) : .Color.white
+                )
+                .padding(
+                    .horizontal,
+                    size == .small ? 18 : 28
+                )
+                .padding(
+                    .vertical,
+                    size == .small ? 8 : 14
+                )
+                .background(disabled ? Color(hex: "D6D6D7") : (type == .primary ? .Color.green : Color(hex: "0ABD4B", alpha: 0.55)))
+                .clipShape(RoundedRectangle(cornerRadius: size == .small ? 12 : 16))
+        }
+        .disabled(disabled)
+    }
+}
+
+#Preview {
+    TapButton(action: {}, text: "Test", type: .primary, size: .large, disabled: .constant(true))
+}
---
+++ Sangle/Extension/Color.swift
@@ -0,0 +1,43 @@
+//
+//  Color.swift
+//  Sangle
+//
+//  Created by  jwkwon0817 on 11/20/24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String, alpha: Double = 1.0) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: alpha
+        )
+    }
+    
+    // Hex 정수값을 사용하는 경우
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
