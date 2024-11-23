b79665489242b1695f3c33a9a7939148ddae54c5
cottons-kr <taeyeong.kim@algorix.io>
2024-11-23 20:50:09 +0900

    feat: updated MyIngredients view

--- Sangle/View/MyIngredients/MyIngredients.swift
+++ Sangle/View/MyIngredients/MyIngredients.swift
@@ -19,38 +19,58 @@ struct MyIngredients: View {
     var body: some View {
         VStack {
             Header(title: "내 식재료", type: .back)
-            ScrollView(.vertical) {
-                filter
-                VStack(spacing: 14) {
-                    ForEach(1...ingredientsCount, id: \.self) { index in
-                        IngredientInfo(content: IngredientModel(name: "고구마", category: "채소", date: Date.from(year: 2024, month: 11, day: 21)), type: .detail, expirationDate: Date.from(year: 2024, month: 12, day: 5))
-                        if (index < ingredientsCount) {
-                            RoundedDivider(height: 1)
+            ZStack {
+                ScrollView(.vertical) {
+                    HStack(spacing: 12) {
+                        filter
+                        storeTip
+                    }
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                    VStack(spacing: 14) {
+                        ForEach(1...ingredientsCount, id: \.self) { index in
+                            IngredientInfo(content: IngredientModel(name: "고구마", category: "채소", date: Date.from(year: 2024, month: 11, day: 21)), type: .detail, expirationDate: Date.from(year: 2024, month: 12, day: 5))
+                            if (index < ingredientsCount) {
+                                RoundedDivider(height: 1)
+                            }
                         }
                     }
+                    .padding(.horizontal, 20)
                 }
-                .padding(.horizontal, 20)
+                .frame(width: .infinity, height: .infinity)
+                
+                scanFAB
             }
-            .frame(width: .infinity, height: .infinity)
         }
     }
     
     var filter: some View {
-        HStack {
-            Spacer()
-            
-            Button(action: {
-                filterMode = filterMode == .recent ? FilterMode.older : FilterMode.recent
-            }) {
-                HStack(spacing: 4) {
-                    Icon("Icon/instant_mix", size: 24)
-                    Text(filterMode == .recent ? "최근순" : "오래된순")
-                        .typography(.body2)
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 14)
+        Button(action: {
+            filterMode = filterMode == .recent ? FilterMode.older : FilterMode.recent
+        }) {
+            HStack(spacing: 4) {
+                Icon("Icon/instant_mix", size: 24)
+                Text(filterMode == .recent ? "최근순" : "오래된순")
+                    .typography(.body2)
+            }
+            .padding(.vertical, 14)
+            .padding(.leading, 20)
+        }
+    }
+    
+    var storeTip: some View {
+        TapButton(action: {}, text: "보관 팁", size: .small, disabled: false)
+    }
+    
+    var scanFAB: some View {
+        VStack() {
+            Button(action: {}) {
+                Badge.Icon(name: "document_scanner")
             }
         }
+        .padding(.trailing)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
     }
 }
 
