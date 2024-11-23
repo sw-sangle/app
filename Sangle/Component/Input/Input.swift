d8017c76c4d96b5061ab7820f85a7425aa48bb97
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-11-23 13:46:18 +0900

    feat: input component

---
+++ Sangle/Component/Input/Input.swift
@@ -0,0 +1,16 @@
+//
+//  Input.swift
+//  Sangle
+//
+//  Created by  jwkwon0817 on 11/23/24.
+//
+
+import SwiftUI
+
+enum InputType {
+    case plain, close, secure
+}
+
+struct Input {
+    
+}
--- Sangle/View/Analysis/AnalysisScreen.swift
+++ Sangle/View/Analysis/AnalysisScreen.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct AnalysisScreen: View {
     @State var path = NavigationPath()
     
+    @State var text: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         NavigationStack(path: $path) {
             ZStack {
                 Color.background
                     .ignoresSafeArea()
                 
-                Text("Analysis")
-                    .foregroundStyle(.black)
+                VStack {
+                    VStack {
+                        Text("일반 Input (filter: s를 포함하는가?)")
+                        Input.Plain(text: $text, placeholder: "닉네임을 입력해주세요", filter: { $0.contains("s")})
+                    }
+                     
+                    VStack {
+                        Text("비밀번호 Input (filter: 6자리인가?)")
+                        Input.Secure(text: $password, placeholder: "비밀번호를 입력해주세요", filter: { $0.count == 6 })
+                    }
+                }
             }
         }
     }
